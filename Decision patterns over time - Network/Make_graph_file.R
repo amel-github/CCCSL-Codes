060f3aa7128d4ea636d7932ab6766ab57004fe40
Elma Dervic <47453391+elmame@users.noreply.github.com>
2020-05-03 01:44:07 +0200

    Final

---
+++ Decision patterns over time - Network/Make_graph_file.R
@@ -0,0 +1,103 @@
+##################################################
+## Project: CCCSL: Complexity Science Hub Covid-19 Control Strategies List (2020)
+## Network - overlap of measures in time
+## Script purpose: make graph file (.gexf)
+## Date:12.04.2020.
+## Authors: Elma Hot Dervic, Nils Haug, Amélie Desvars-Larrive
+##################################################
+
+
+library(dplyr)
+library(stats)
+library(mosaicData)
+library(Matrix)
+library(lattice)
+library(mosaic)
+library(rgexf)
+library(stringr)
+
+edges_m <- as.matrix(which(Overlap>0, arr.ind = T))
+edgesWeight <- Overlap[Overlap>0]
+str(edges_m)
+
+edges_m <- edges_m[complete.cases(edges_m),]
+edgesWeight <- edgesWeight[complete.cases(edgesWeight)]
+str(edges_m)
+str(edgesWeight)
+
+
+edges_all_filter <- cbind(edges_m, edgesWeight)
+str(edges_all_filter)
+edges_all_filter <- as.data.frame(edges_all_filter)
+str(edges_all_filter)
+names(edges_all_filter) <- c("Source", "Target", "edgesWeight")
+
+# sum(duplicated(edges_all_filter[,c(1,2)]))
+
+edges_all_filter <- edges_all_filter[complete.cases(edges_all_filter),]
+# sum(duplicated(edges_all_filter[,c(1,2)]))
+# str(edges_all_filter)
+edges_all_filter <- edges_all_filter[edges_all_filter$edgesWeight>0.5,]
+
+# SCALE LINKS WEIGHT
+# edges_all_filter$edgesWeight <- edges_all_filter$edgesWeight/max(edges_all_filter$edgesWeight)*10
+
+
+# make data frames for write.gexf funtion
+# edges - two colums data frame
+edges <- edges_all_filter[, c(1,2)]
+str(edges)
+
+
+str(nodes)
+str(nodes_color)
+str(nodes_size)
+str(position)
+str(node_att_names_l1)
+
+# SCALE SIZE of nodes
+nodes_size <- nodes_size/max(nodes_size)*5
+# change min size od nodes
+# make small nodes visiable in gexf
+# nodes_size <- nodes_size + 5
+
+
+
+# make GEXF file
+g1 <- write.gexf(nodes, edges,  edgesWeight = edges_all_filter$edgesWeight, nodesVizAtt = list( color = nodes_color, size=nodes_size,  position=position), nodesAtt = node_att_names_l1, defaultedgetype = "directed")
+# save GEXF file
+f <- file("Overlap_measure_graph.gexf")
+writeLines(g1$graph, con = f)
+close(f)
+
+
+# ************************* FILTER LINKS *************************
+# filter links bigger links 
+# make data frames for write.gexf funtion
+# edges - two colums data frame
+
+
+nodes <- nodes[nodes_size>2,]
+nodes_color <- nodes_color[nodes_size>2,]
+node_att_names_l1 <- node_att_names_l1[nodes_size>2,]
+position <- position[nodes_size>2,]
+nodes_size <- nodes_size[nodes_size>2]
+
+
+
+edges_all_filter1 <- edges_all_filter[edges_all_filter$Source %in%  nodes$id & edges_all_filter$Target %in%  nodes$id,]
+str(edges_all_filter1)
+
+
+edges_all_filter1 <- edges_all_filter1[edges_all_filter1$edgesWeight>0.5,]
+edges <- edges_all_filter1[, c(1,2)]
+str(edges)
+
+
+g1 <- write.gexf(nodes, edges,  edgesWeight = edges_all_filter1$edgesWeight, nodesVizAtt = list( color = nodes_color, size=nodes_size, position=position), nodesAtt = node_att_names_l1, defaultedgetype = "directed")
+
+f <- file("Overlap_Filtered_measure_graph.gexf")
+writeLines(g1$graph, con = f)
+close(f)
+
+
---
+++ Decision patterns over time - Network/Make_nodes.R
@@ -0,0 +1,101 @@
+##################################################
+## Project: CCCSL: Complexity Science Hub Covid-19 Control Strategies List (2020)
+## Network - overlap of measures in time
+## Script purpose: This scripts make nodes for the network
+## Date:12.04.2020.
+## Authors: Elma Hot Dervic, Nils Haug, Amélie Desvars-Larrive
+##################################################
+
+
+library(dplyr)
+library(stats)
+library(mosaicData)
+library(Matrix)
+library(lattice)
+library(mosaic)
+library(rgexf)
+library(stringr)
+
+
+# ************************* NODES *************************
+numer_of_measures <- length(names_l2)
+nodes <- NULL
+nodes$label <- names_l2
+nodes$id <- c(1:numer_of_measures)
+nodes <- as.data.frame(nodes)
+str(nodes)
+# first column ID, second column LABEL
+nodes <- nodes[,c(2,1)]
+nodes$label <- as.character(nodes$label)
+
+
+# set colors based on L1 level
+# x size of each L1 level
+x <- table(names_l1)
+x <- unname(x)
+
+# define colors
+r <- c(0, 88, 150, 180, 191, 255, 255)
+g <- c(0, 242, 29, 202, 0, 128, 255)
+b <- c(255, 26, 26, 242, 255, 0, 0)
+
+nodes_color_r <- NULL
+nodes_color_g <- NULL
+nodes_color_b <- NULL
+for(i in 1:length(unique(names_l1))){
+nodes_color_r <- c(nodes_color_r, rep(r[i], x[i]) )
+nodes_color_g <-  c(nodes_color_g, rep(g[i], x[i]) )
+nodes_color_b <- c(nodes_color_b, rep(b[i], x[i]) )
+}
+nodes_color <- data.frame( r=nodes_color_r, g = nodes_color_g, b = nodes_color_b)
+# nodes_color
+nodes_color$alpha <- rep(1, numer_of_measures)
+nodes_color <- as.data.frame(nodes_color)
+str(nodes_color)
+
+# count number of countries for each measure
+# OPTIMIZE
+matrix_for_size  <- rep(0, numer_of_measures)
+for(i in countries_with_measures){
+  onecountry_measures <- measures[measures$Country==i,]
+  onecountry_measures <- unname(onecountry_measures[,3:dim(measures)[2]])
+  matrix_for_size[which(colSums(onecountry_measures)>0)] <- matrix_for_size[which(colSums(onecountry_measures)>0)] + 1
+}
+nodes_size <- matrix_for_size + 1
+
+# read positions 
+# save positions from GEPHI file
+# number of nodes: 77
+
+position <- readRDS("NODES_position_from_Gephi.rds")
+position <- position[order(position$id),]
+position$id <- NULL
+position$label_old <- NULL
+position$z <- rep(0, nrow(position))
+names(position) <- c("x", "y", "z")
+position$x <- as.numeric(position$x)
+position$y <- as.numeric(position$y)
+str(position )
+
+
+str(nodes)
+str(nodes_color)
+str(nodes_size)
+str(position)
+
+# add one more attribute to nodes
+node_att_names_l1 <- NULL
+node_att_names_l1$L1 <- as.character(names_l1)
+node_att_names_l1$id <- c(1:length(names_l1))
+node_att_names_l1 <-  as.data.frame(node_att_names_l1)
+node_att_names_l1$L1 <- as.character(node_att_names_l1$L1)
+str(node_att_names_l1)
+
+
+
+str(nodes)
+str(nodes_color)
+str(nodes_size)
+str(position)
+str(node_att_names_l1)
+
