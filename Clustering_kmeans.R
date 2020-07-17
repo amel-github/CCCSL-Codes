3d431dc1878c2074df86de004b7698e784ba84cc
Elma Dervic <47453391+elmame@users.noreply.github.com>
2020-07-17 13:59:05 +0200

    Elma's version

--- Clustering_kmeans.R
+++ Clustering_kmeans.R
@@ -4,6 +4,7 @@
 ## Script purpose: K-means clustering to cluster countries based on the timeline of the implemented measures
 ## Date:12.07.2020.
 ## Authors: Elma Hot Dervic, Nils Haug, Amélie Desvars-Larrive
+## Modified by David Garcia
 ##################################################
 
 library(tidyverse)
